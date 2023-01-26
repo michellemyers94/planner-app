65aa01b85cf15c57ba04dd6d62f692910ddd116f
Michelle Myers <michelleliumyers@gmail.com>
2023-01-25 23:41:46 -0800

    first commit

---
+++ src/components/Todos.svelte
@@ -0,0 +1,103 @@
+
+
+<script>
+export let todos = [];
+$: totalTodos = todos.length;
+$: completedTodos = todos.filter((todo) =>
+    todo.completed).length;
+
+function removeTodo(todo) {
+    todos = todos.filter((t) => t.id !== todo.id)
+}
+
+
+
+let newTodoName = ''
+
+function addTodo() {
+    todos = [...todos, { id: newTodoId, name: newTodoName, completed: false }]
+    newTodoName = ''
+}
+
+
+let newTodoId
+$: {
+    if (totalTodos === 0) {
+        newTodoId = 1;
+    } else {
+        newTodoId = Math.max(...todos.map((t) => t.id)) + 1;
+    }
+}
+
+
+
+let filter = 'all'
+const filterTodos = (filter, todos) =>
+    filter === 'active' ? todos.filter((t) => !t.completed) :
+        filter === 'completed' ? todos.filter((t) => t.completed) :
+            todos
+
+</script>
+
+<form on:submit|preventDefault={addTodo}>
+<div class="task-block">
+
+
+
+
+<!--add task    -->
+
+    <h2 id="list-heading">{completedTodos} out of {totalTodos} items completed</h2>
+
+
+<div class="tab">
+    <h4 class="tab-title">
+        OSU Courses
+    </h4>
+    </div>
+    <ul class="task-block">
+        {#each todos as todo (todo.id)}
+
+            <li class="todo">
+                <input type="checkbox" class="checkmark" id="todo-{todo.id}"
+                       on:click={() => todo.completed = !todo.completed}
+                       checked={todo.completed}
+                />
+
+                <label for="todo-{todo.id}" class="">
+                    {todo.name}
+                </label>
+
+            <div class="edit-delete-item-group">
+                <button type="button" class="edit-task">edit</button>
+                <button type="button" class="delete-task"
+                        on:click={() => removeTodo(todo)}
+                >
+                    delete <span class="visually-hidden"></span>
+                </button>
+
+            </div>
+            </li>
+
+            {:else}
+            <li>Add a task to get started!</li>
+            {/each}
+
+    </ul>
+
+
+
+
+<div class="add-task-area">
+    <input bind:value={newTodoName}
+           type="text"
+           id="todo-0"
+           autocomplete="off"
+           class="" />
+    <button type="submit" disabled="" class="add-task">
+        + add task
+    </button>
+    </div>
+</div>
+</form>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Todos from "../components/Todos.svelte";
+    import './styles.css';
+
+    let todos = [
+        { id: 1, name: "Complete Assignment 1", completed: true },
+        { id: 2, name: "Complete Assignment 2", completed: true },
+        { id: 3, name: "Complete Assignment 3", completed: false }
+    ];
+</script>
+
+
+<div class="grid-container">
+
+<!--    left column-->
+    <div class="nav-column">
+
+        <h1 class="app-title">Planner App</h1>
+        <h3 class="categories-title">categories</h3>
+
+
+        <div class="category-buttons">
+        <button class="add-category">+ add category</button>
+        <button class="edit-category">- edit category</button>
+            </div>
+    </div>
+
+
+
+<!--    right column-->
+    <div class="main-body">
+        <h1>Task Overview</h1>
+        <Todos {todos} />
+    </div>
+
+
+
+
+</div>
